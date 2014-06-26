72e910559cec1799ee3c648509ff1dccc4ba0882
Lukas Streiter <buffaloluk7@gmail.com>
2014-06-26 12:08:55 +0200

    Enhanced exception logging, added generateKeypair method

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import sun.util.logging.resources.logging_es;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,64 +24,110 @@ public class Controller {
 	private TextArea taLog;
 	@FXML
 	private TextField tfPassword;
+
 	private RSA rsa;
 
-	public Controller() throws RSAException {
-		rsa = RSA.newInstance();
-	}
-
 	@FXML
-	protected void btnEncryptMessageAction(ActionEvent event) throws RSAException {
+	protected void btnEncryptMessageAction(ActionEvent event) {
+		if (rsa == null) {
+			log("Generate a key pair first or import an existing one!");
+			return;
+		}
+
 		if (taMessage.getText().isEmpty() || taMessage.getText().length() > 190) {
 			log("Text empty or too long!");
 			return;
 		}
 
-		taResult.setText(rsa.encrypt(taMessage.getText()));
+		try {
+			taResult.setText(rsa.encrypt(taMessage.getText()));
+		} catch (RSAException e) {
+			logException(e.getMessage());
+			return;
+		}
 
 		log("Text encrypted!");
 	}
 
 	@FXML
-	protected void btnDecryptMessageAction(ActionEvent event) throws RSAException {
+	protected void btnDecryptMessageAction(ActionEvent event) {
+		if (rsa == null) {
+			log("Generate a key pair first or import an existing one!");
+			return;
+		}
+
 		if (taMessage.getText().isEmpty()) {
 			log("Text empty!");
 			return;
 		}
 
-		taResult.setText(rsa.decrypt(taMessage.getText()));
+		try {
+			taResult.setText(rsa.decrypt(taMessage.getText()));
+		} catch (RSAException | IllegalArgumentException e) {
+			logException(e.getMessage());
+			return;
+		}
 
 		log("Text decrypted!");
 	}
 
 	@FXML
-	protected void btnExportKeys(ActionEvent event) throws IOException, KeyStorageException, DESException {
+	protected void btnGenerateKeys(ActionEvent event) {
+		try {
+			rsa = RSA.newInstance();
+		} catch (RSAException e) {
+			logException(e.getMessage());
+			return;
+		}
+
+		log("New key pair generated");
+	}
+
+	@FXML
+	protected void btnExportKeys(ActionEvent event) {
+		if (rsa == null) {
+			log("Generate a key pair first or import an existing one!");
+			return;
+		}
+
 		if (tfPassword.getText().isEmpty() || tfPassword.getText().length() < 8) {
 			log("Password empty or too short (minimum 8 chars)!");
 			return;
 		}
-		KeyStorage.writePrivateKey("private.key", rsa.getKeyPair().getPrivate(), DES.newInstance(tfPassword.getText()));
-		Files.write(new File("public.key").toPath(), rsa.getKeyPair().getPublic().getEncoded(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+
+		try {
+			KeyStorage.writePrivateKey("private.key", rsa.getKeyPair().getPrivate(), DES.newInstance(tfPassword.getText()));
+			Files.write(new File("public.key").toPath(), rsa.getKeyPair().getPublic().getEncoded(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+		} catch (DESException | IOException | KeyStorageException e) {
+			logException(e.getMessage());
+			return;
+		}
 
 		log("Keys exported!");
 	}
 
 	@FXML
-	protected void btnImportKeys(ActionEvent event) throws DESException, RSAException, KeyStorageException, NoSuchAlgorithmException, IOException, InvalidKeySpecException {
+	protected void btnImportKeys(ActionEvent event) {
 		if (tfPassword.getText().isEmpty() || tfPassword.getText().length() < 8) {
 			log("Password empty or too short (minimum 8 chars)!");
 			return;
 		}
 
-		PrivateKey privateKey = KeyStorage.readPrivateKey("private.key", DES.newInstance(tfPassword.getText()));
+		try {
+			PrivateKey privateKey = KeyStorage.readPrivateKey("private.key", DES.newInstance(tfPassword.getText()));
 
-		PublicKey publicKey = KeyFactory
-				.getInstance("RSA")
-				.generatePublic(new X509EncodedKeySpec(Files.readAllBytes(new File("public.key").toPath())));
+			PublicKey publicKey = KeyFactory
+					.getInstance("RSA")
+					.generatePublic(new X509EncodedKeySpec(Files.readAllBytes(new File("public.key").toPath())));
 
-		KeyPair keyPair = new KeyPair(publicKey, privateKey);
+			KeyPair keyPair = new KeyPair(publicKey, privateKey);
 
-		rsa = RSA.newInstance(keyPair);
+			rsa = RSA.newInstance(keyPair);
+		} catch (RSAException | InvalidKeySpecException | NoSuchAlgorithmException | IOException | KeyStorageException | DESException e) {
+			logException(e.getMessage());
+			return;
+		}
+
 
 		log("Keys imported");
 	}
@@ -92,5 +139,10 @@ public class Controller {
 
 	private void log(String text) {
 		taLog.appendText(text + "\n");
+	}
+
+	private void logException(String message) {
+		String errorMessageOnly = message.substring(message.lastIndexOf(":") + 1, message.length());
+		log(errorMessageOnly.trim());
 	}
 }
